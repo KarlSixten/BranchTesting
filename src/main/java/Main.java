78fff1298d81445f58e932283b9f399189104aa7
Karl Sixten Bjarnø <karlsixten@gmail.com>
2023-11-13 09:28:53 +0100

    Test of makeSound() in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        System.out.println(dog.makeSound());
+        System.out.println(cat.makeSound());
+    }
 }
